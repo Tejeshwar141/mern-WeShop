8d1f15166132a597a91ee6d62983318d07a9c8e5
Tejeshwar141 <tejesh9790@gmail.com>
2022-09-08 11:58:30 +0530

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">WeShop</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
